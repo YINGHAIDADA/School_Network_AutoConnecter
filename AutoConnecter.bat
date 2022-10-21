f05fe4e41c68686eca59ecb695f7013a049cddb5
YINGHAI <49584621+YINGHAIDADA@users.noreply.github.com>
2022-10-21 09:42:57 +0800

    隐藏CMD
    Signed-off-by: YINGHAI <49584621+YINGHAIDADA@users.noreply.github.com>

--- AutoConnecter.bat
+++ AutoConnecter.bat
@@ -1,4 +1,8 @@
 @echo off
 
-cd /d %~dp0
+if "%1" == "h" goto begin
+mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+:begin
+REM
+
 python AutoConnect.py脚本绝对路径 校园网网址 登录用户名 密码
